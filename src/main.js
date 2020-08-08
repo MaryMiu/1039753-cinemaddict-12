3573a73aa6a197b05de33246366c927c5a4b286b
MaryMiu <m.zhivilo@gmail.com>
2020-08-08 15:54:26 +0700

    Убирает лишний рендер

--- src/main.js
+++ src/main.js
@@ -1,17 +1,34 @@
-
 const FILM_COUNT = 5;
 const EXTRA_COUNT = 2;
 const FILM_COUNT_EXTRA = 2;
 
-import {createUserBar} from "./view/user.js";
-import {creareSiteMenu} from "./view/menu.js";
-import {createSort} from "./view/sort.js";
-import {createFilmContainer} from "./view/container.js";
-import {createFilmCard} from "./view/card.js";
-import {createLoadButton} from "./view/load-button.js";
-import {createFilmListExtra} from "./view/list-extra.js";
-import {createFilmCardExtra} from "./view/card-extra.js";
-import {createStatistics} from "./view/statistics.js";
+import {
+  createUserBar
+} from "./view/user.js";
+import {
+  creareSiteMenu
+} from "./view/menu.js";
+import {
+  createSort
+} from "./view/sort.js";
+import {
+  createFilmContainer
+} from "./view/container.js";
+import {
+  createFilmCard
+} from "./view/card.js";
+import {
+  createLoadButton
+} from "./view/load-button.js";
+import {
+  createFilmListExtra
+} from "./view/list-extra.js";
+import {
+  createFilmCardExtra
+} from "./view/card-extra.js";
+import {
+  createStatistics
+} from "./view/statistics.js";
 
 const render = (container, position, template) => {
   container.insertAdjacentHTML(position, template);
@@ -39,13 +56,17 @@ for (let i = 0; i < EXTRA_COUNT; i++) {
   render(films, `beforeend`, createFilmListExtra());
 }
 
+let filmCardsExtra = ``;
+
+for (let i = 0; i < FILM_COUNT_EXTRA; i++) {
+  filmCardsExtra += createFilmCardExtra();
+}
+
 const filmListsExtra = siteMain.querySelectorAll(`.films-list--extra`);
 
 filmListsExtra.forEach((item) => {
   const filmContainerExtra = item.querySelector(`.films-list__container`);
-  for (let i = 0; i < FILM_COUNT_EXTRA; i++) {
-    render(filmContainerExtra, `beforeend`, createFilmCardExtra());
-  }
+  render(filmContainerExtra, `beforeend`, filmCardsExtra);
 });
 
 const statistics = document.querySelector(`.footer__statistics`);
